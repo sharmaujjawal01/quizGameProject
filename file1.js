512350402504b8125fc25a09d92416e310966485
Ujjawal Sharma <45787300+sharmaujjawal01@users.noreply.github.com>
2021-04-07 16:02:11 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How old a person is termed as adult?",
+    answer: "18 years and above",
+    options: [
+      "18 years and above",
+      "6 months and above",
+      "25 years and below",
+      "60 years and above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the capital of India?",
+    answer: "New Delhi",
+    options: [
+      "Bengaluru",
+      "Mumbai",
+      "New Delhi",
+      "Chennai"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How long is 1 meter?",
+    answer: "100 cm",
+    options: [
+      "10 km",
+      "100 cm",
+      "15 ft.",
+      "0.1 cm"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which is the tallest statue in the world?",
+    answer: "statue of unity",
+    options: [
+      "statue of liberty",
+      "statue of unity",
+      "Hanuman's statue",
+      "Jesus statue"
+    ]
+  },
+ 
+];
